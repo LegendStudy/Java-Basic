0c107c89b73ee2f67d249dfe2754c62606f513b6
Gyehwan Baek <b99hwan@gmail.com>
2024-01-09 16:39:55 +0900

    [Week1][Chap4]생성자-오버로딩과 this()(#7)

--- 백계환/java-basic2/src/construct/MemberConstruct.java
+++ 백계환/java-basic2/src/construct/MemberConstruct.java
@@ -6,9 +6,9 @@ public class MemberConstruct {
     int grade;
 
     //추가
-//    MemberConstruct(String name, int age) {
-//        this(name, age, 50); //변경
-//    }
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); //변경
+    }
 
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ",age=" + age + ",grade=" + grade);
