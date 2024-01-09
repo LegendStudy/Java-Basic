0db9d1488490303a0b2b7dff7d85d146e5296018
Gyehwan Baek <b99hwan@gmail.com>
2024-01-09 18:43:55 +0900

    [Week1][Chap1~5] Baekgyehwan (#10)
    * [Week1][Chap1]배열도입-리팩토링(#7)
    
    * [Week1][Chap1]문제와 풀이(#7)
    
    * [Week1][Chap2]기본형vs참조형2-변수대입(#7)
    
    * [Week1][Chap2]기본형vs참조형3-메서드 호출
    
    * [Week1][Chap2]참조형과 메서드 호출-활용(#7)
    
    * [Week1][Chap2]변수와 초기화(#7)
    
    * [Week1][Chap2]null(#7)
    
    * [Week1][Chap2]NullPointerException(#7)
    
    * [Week1][Chap2]문제와 풀이(#7)
    
    * [Week1][Chap3]절차 지향 프로그래밍1-시작(#7)
    
    * [Week1][Chap3]절차 지향 프로그래밍2-데이터 묶음(#7)
    
    * [Week1][Chap3]절차 지향 프로그래밍3-메서드 추출(#7)
    
    * [Week1][Chap3]클래스와 메서드(#7)
    
    * [Week1][Chap3]객체 지향 프로그래밍(#7)
    
    * [Week1][Chap3]문제와 풀이(#7)
    
    * [Week1][Chap4]생성자-필요한 이유(#7)
    
    * [Week1][Chap4]this(#7)
    
    * [Week1][Chap4]생성자-도입(#7)
    
    * [Week1][Chap4]기본 생성자(#7)
    
    * [Week1][Chap4]생성자-오버로딩과 this()(#7)
    
    * [Week1][Chap4]문제와 풀이(#7)
    
    * [Week1][Chap5]패키지-시작(#7)
    
    * [Week1][Chap5]패키지-import(#7)
    
    * [Week1][Chap5]패키지 활용(#7)

---
+++ 백계환/java-basic2/src/ref/MethodChange1.java
@@ -0,0 +1,14 @@
+package ref;
+
+public class MethodChange1 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("메서드 호출 전: a = " + a);
+        changePrimitive(a);
+        System.out.println("메서드 호출 후: a = " + a);
+    }
+
+    static void changePrimitive(int x) {
+        x = 20; // 메서드가 종료되면 매개변수 x는 제거된다.
+    }
+}
