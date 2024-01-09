c0d9c53403d88667e9a405e102d3aba4c3bfab79
arinming <jyandysys1@naver.com>
2024-01-09 17:15:56 +0900

    [Week1][Chap5] 패키지 - import (#4)

---
+++ 김아린/java-basic/src/pack/PackageMain3.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+    public static void main(String[] args) {
+        User userA = new User();
+        pack.b.User userB = new pack.b.User();  // 둘 중에 하나만 임포트
+    }
+}
