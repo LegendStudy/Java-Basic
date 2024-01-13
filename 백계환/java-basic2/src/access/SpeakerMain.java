b6154ac5ff407fcdfdcd1e896e4e804b1e759c03
Gyehwan Baek <b99hwan@gmail.com>
2024-01-13 22:34:21 +0900

    [Week2][Chap6]접근 제어자 이해2(#17)

--- 백계환/java-basic2/src/access/SpeakerMain.java
+++ 백계환/java-basic2/src/access/SpeakerMain.java
@@ -11,7 +11,8 @@ public class SpeakerMain {
         speaker.showVolume();
 
         //필드에 직접 접근
-        System.out.println("volume 필드 직접 접근 수정"); speaker.volume = 200;
+        System.out.println("volume 필드 직접 접근 수정");
+//        speaker.volume = 200;
         speaker.showVolume();
     }
 }
