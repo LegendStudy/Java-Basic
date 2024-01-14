7b0cfbd0ce9bbb46f7a41437dc51e9aa68e1536f
arinming <jyandysys1@naver.com>
2024-01-14 12:43:47 +0900

    [Week2][Chap7] static 변수3 (#15)

--- 김아린/java-basic/src/static1/DataCountMain3.java
+++ 김아린/java-basic/src/static1/DataCountMain3.java
@@ -10,5 +10,18 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count = " + Data3.count);
+
+        // 추가
+        // 인스턴스를 통한 접근
+        // 권장하지 않음 -> 인스턴스 변수라고 오해할 수 있기 때문
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        Data3 data5 = new Data3("E");
+        System.out.println(data5.count);
+
+        // 클래스를 통한 접근
+        // 권장 -> 클래스 변수임이 직관적으로 보임
+        System.out.println(Data3.count);
     }
 }
