46aed4136c0dfca18df59cee2a175bbffdd48800
toychip <dlawnsgud427@naver.com>
2024-01-15 13:42:53 +0900

    [Week2][Chap6] 접근 제어자 이해와 사용 (#14)

---
+++ 임준형/java-basic/src/main/java/week2/access/b/AccessOuterMain.java
@@ -0,0 +1,22 @@
+package week2.access.b;
+
+import week2.access.a.AccessData;
+
+public class AccessOuterMain {
+
+    public static void main(String[] args) {
+        AccessData accessData = new AccessData();
+
+        // public
+        accessData.publicField = 1;
+        accessData.publicMethod();
+
+        // 다른 패키지, default 호출 불가
+//        accessData.defaultField = 2;
+//        accessData.defaultMethod();
+
+        // private 호출 불가
+
+        accessData.innerAccess();
+    }
+}
