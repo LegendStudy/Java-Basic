56e1e5664c84514afd235bb6444311899294e058
Gyehwan Baek <b99hwan@gmail.com>
2024-01-16 17:48:07 +0900

    [Week2][Chap7]static 변수3(#17)

--- 백계환/java-basic2/src/static1/DataCountMain3.java
+++ 백계환/java-basic2/src/static1/DataCountMain3.java
@@ -11,5 +11,13 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count=" + Data3.count);
+
+        //추가
+        //인스턴스를 통한 접근
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count); // 권장되지 않음. 인스턴스 변수인 것 같은 착각을 준다.
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
     }
 }
