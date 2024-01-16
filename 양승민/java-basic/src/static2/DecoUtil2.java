d957066ea77c38651697288aaf5ae76d2ca89bb8
Luke_YS <yjsmk0902@gmail.com>
2024-01-17 00:40:17 +0900

    [Week2][Chap7] static 메서드1 (#16)

--- 양승민/java-basic/src/static2/DecoUtil2.java
+++ 양승민/java-basic/src/static2/DecoUtil2.java
@@ -1,7 +1,7 @@
 package static2;
 
 public class DecoUtil2 {
-    public static String deco(String string) {
+    public static String deco(String str) {
         String result = "*" + str + "*";
         return result;
     }
