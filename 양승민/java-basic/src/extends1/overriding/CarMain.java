ae0f03640702b3b83601de0d3f02637543675c44
Luke_YS <yjsmk0902@gmail.com>
2024-01-17 12:13:31 +0900

    [Week2][Chap9] 상속과 메서드 오버라이딩 (#16)

--- 양승민/java-basic/src/extends1/overriding/CarMain.java
+++ 양승민/java-basic/src/extends1/overriding/CarMain.java
@@ -4,7 +4,7 @@ public class CarMain {
     public static void main(String[] args) {
         ElectricCar electricCar = new ElectricCar();
         electricCar.move();
-        
+
         GasCar gasCar = new GasCar();
         gasCar.move();
     }
