a969c52a89cc4b958cf81f3e7081d8beb84b69ea
arinming <jyandysys1@naver.com>
2024-01-31 23:20:02 +0900

    [Week3][Chap10] 다운캐스팅과 주의점 (#27)

--- 김아린/java-basic/src/poly/basic/CastingMain4.java
+++ 김아린/java-basic/src/poly/basic/CastingMain4.java
@@ -8,7 +8,7 @@ public class CastingMain4 {
         child1.childMethod();   // 문제 없음
 
         Parent parent2 = new Parent();
-        
+
         Child child2 = (Child) parent2; // 런타임 오류 - ClassCastException
         child2.childMethod();   // 실행 불가
     }
