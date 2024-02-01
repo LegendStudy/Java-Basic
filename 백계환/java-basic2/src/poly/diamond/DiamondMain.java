88dd0e43399e9abda557dd8feab6c867b8ad0c5a
Gyehwan Baek <b99hwan@gmail.com>
2024-02-01 18:06:17 +0900

    [Week3][Chap11]인터페이스-다중구현

---
+++ 백계환/java-basic2/src/poly/diamond/DiamondMain.java
@@ -0,0 +1,15 @@
+package poly.diamond;
+
+public class DiamondMain {
+
+    public static void main(String[] args) {
+
+        InterfaceA interfaceA = new Child();
+        interfaceA.methodA();
+        interfaceA.methodCommon();
+
+        InterfaceB interfaceB = new Child();
+        interfaceB.methodB();
+        interfaceB.methodCommon();
+    }
+}
