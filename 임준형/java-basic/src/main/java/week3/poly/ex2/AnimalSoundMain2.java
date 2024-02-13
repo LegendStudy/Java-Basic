108dd7e2db89d2dbf81c3528509e4655b6b0fed2
toychip <dlawnsgud427@naver.com>
2024-02-14 00:00:58 +0900

    [Week3][Chap11] 다형성 활용3 (#26)

---
+++ 임준형/java-basic/src/main/java/week3/poly/ex2/AnimalSoundMain2.java
@@ -0,0 +1,21 @@
+package week3.poly.ex2;
+
+public class AnimalSoundMain2 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        Animal[] animals = {dog, cat, caw};
+
+        for (Animal animal : animals) {
+            soundAnimal(animal);
+        }
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
