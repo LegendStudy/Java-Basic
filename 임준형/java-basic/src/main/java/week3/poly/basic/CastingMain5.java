19abd306f6e54e66299ca10b8bf1bcb855e1125e
toychip <dlawnsgud427@naver.com>
2024-02-13 21:44:29 +0900

    [Week3][Chap10] instanceof (#26)

---
+++ 임준형/java-basic/src/main/java/week3/poly/basic/CastingMain5.java
@@ -0,0 +1,27 @@
+package week3.poly.basic;
+
+// 다운캐스팅을 자동으로 하지 않는 이유
+public class CastingMain5 {
+
+    public static void main(String[] args) {
+        Parent parent1 = new Parent();
+        call(parent1);
+        System.out.println("-------------");
+
+        Parent parent2 = new Child();
+        call(parent2);
+    }
+
+    private static void call(Parent parent) {
+        parent.parentMethod();
+
+        if (parent instanceof Child) {
+            System.out.println("Child 인스턴스 맞음");
+            Child child = (Child) parent;
+            child.childMethod();
+        } else {
+            System.out.println("Child 인스턴스 아님");
+        }
+    }
+
+}
